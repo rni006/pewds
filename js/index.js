396128200786f82a28b0f0524c413abf463f5728
Renate <Renate.Nikolajeva@student.uib.no>
2019-03-03 13:53:42 +0100

    lets GOOO it work
    ye

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-$('document').ready(function() {
-	var options = { videoId: 'https://youtu.be/6Dh-RL__uN4', start: 3 };
+$('test.html').ready(function() {
+	var options = { videoId: '6Dh-RL__uN4', start: 3 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
